f91223e2a5edd79023b2d5fcbb9b80f911489250
Chirag <chiragmndl@gmail.com>
2025-06-04 03:04:44 +0530

    Update index.js
    statically serve client

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const identifyRouter = require('./src/routes/identity');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+//cors
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
 
 app.use('/identify', identifyRouter);
+
+// Serve static files and SPA
+const clientPath = path.join(__dirname,'client', 'build');
+app.use(express.static(clientPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
